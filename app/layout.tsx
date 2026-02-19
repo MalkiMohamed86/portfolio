9e693e04e749dfd7088b16f765aa80a5961055ff
MalkiMohamed <165523413+MalkiMohamed86@users.noreply.github.com>
2026-02-19 16:38:58 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ 
@@ -34,7 +34,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         {children}
-        <Analytics />
+          <Analytics />
       </body>
     </html>
   );
